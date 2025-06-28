1222a60e63315a0f13cfd92b62f2570651f2c1af
Matt Sherman <mwsherman@gmail.com>
2025-06-28 19:49:22 -0400

    fix max tokens bug

--- bucket.go
+++ bucket.go
@@ -22,19 +22,34 @@ func (b *bucket) allow(now time.Time, limit limit) bool {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
+	b.checkMaxTokens(now, limit)
+
 	// Check if enough time has passed such that there is at least one token
 	allow := b.time.Before(now.Add(-limit.durationPerToken))
 	if allow {
 		b.consumeToken(limit)
 	}
+
 	return allow
 }
 
+// consumeToken removes a token from the bucket
 func (b *bucket) consumeToken(limit limit) {
 	b.time = b.time.Add(limit.durationPerToken)
 }
 
 // remainingTokens returns the number of tokens remaining in the bucket
+// only call it for testing our math
 func (b *bucket) remainingTokens(now time.Time, limit limit) int64 {
+	b.checkMaxTokens(now, limit)
 	return int64(now.Sub(b.time) / limit.durationPerToken)
+}
+
+func (b *bucket) checkMaxTokens(now time.Time, limit limit) {
+	// If the bucket is older than the limit period, reset it
+	// Otherwise, it will represent too many tokens
+	min := now.Add(-limit.Period)
+	if b.time.Before(min) {
+		b.time = min
+	}
 }
--- bucket_test.go
+++ bucket_test.go
@@ -1,7 +1,6 @@
 package ratelimiter
 
 import (
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -14,20 +13,31 @@ func TestBucket_RemainingTokens(t *testing.T) {
 	limit := NewLimit(9, time.Second)
 	bucket := newBucket(now, limit)
 
-	actual := bucket.remainingTokens(now, limit)
-	expected := int64(9)
-	require.Equal(t, expected, actual, "remaining tokens should equal to limit count")
+	{
+		actual := bucket.remainingTokens(now, limit)
+		expected := limit.Count
+		require.Equal(t, expected, actual, "remaining tokens should equal to limit count")
+	}
+
+	now = now.Add(time.Hour)
+	{
+		actual := bucket.remainingTokens(now, limit)
+		expected := limit.Count
+		require.Equal(t, expected, actual, "remaining tokens should equal to limit count after a long time")
+	}
 }
 
 func TestBucket_ConsumeToken(t *testing.T) {
 	now := time.Now()
 	limit := NewLimit(9, time.Second)
 	bucket := newBucket(now, limit)
-	bucket.consumeToken(limit)
 
-	actual := bucket.remainingTokens(now, limit)
-	expected := int64(8)
-	require.Equal(t, expected, actual, "remaining tokens should be one less after consumption")
+	for i := range limit.Count {
+		bucket.consumeToken(limit)
+		actual := bucket.remainingTokens(now, limit)
+		expected := limit.Count - i - 1
+		require.Equal(t, expected, actual, "remaining tokens should be one less after consumption")
+	}
 }
 
 func TestBucket_Allow(t *testing.T) {
@@ -37,7 +47,7 @@ func TestBucket_Allow(t *testing.T) {
 	limit := NewLimit(9, time.Second)
 	bucket := newBucket(now, limit)
 
-	for range 9 {
+	for range limit.Count {
 		actual := bucket.allow(now, limit)
 		require.True(t, actual, "expected to allow request when tokens are available")
 		now = now.Add(time.Millisecond)
@@ -47,9 +57,9 @@ func TestBucket_Allow(t *testing.T) {
 	actual := bucket.allow(now, limit)
 	require.False(t, actual, "expected to deny request after tokens are exhausted")
 
-	now = now.Add(time.Millisecond * 120)
+	now = now.Add(time.Millisecond * 112)
 
-	// A new token should have refilled by now
+	// One new token should have refilled by now
 	actual = bucket.allow(now, limit)
 	require.True(t, actual, "expected to allow request after waiting for refill")
 }
@@ -67,10 +77,7 @@ func TestBucket_Allow_Concurrent(t *testing.T) {
 		wg.Add(1)
 		go func(index int) {
 			defer wg.Done()
-			// Add jitter to the timestamp
-			jitter := time.Duration(rand.Intn(10)) * time.Millisecond
-			timestamp := now.Add(jitter)
-			actual := bucket.allow(timestamp, limit)
+			actual := bucket.allow(now, limit)
 			require.True(t, actual, "expected to allow request when tokens are available (goroutine %d)", index)
 		}(i)
 	}

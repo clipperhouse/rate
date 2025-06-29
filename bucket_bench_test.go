ec857383b84d902865f0607e7612e3cc9b403133
Matt Sherman <mwsherman@gmail.com>
2025-06-29 11:22:38 -0400

    Add benchmarks with x/rate (#4)

---
+++ bucket_bench_test.go
@@ -0,0 +1,34 @@
+package ratelimiter
+
+import (
+	"testing"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+// Benchmark single allow calls for this package
+func BenchmarkAllow_Single(b *testing.B) {
+	lim := NewLimit(100, time.Second)
+	bucket := newBucket(time.Now(), lim)
+	now := time.Now()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for b.Loop() {
+		bucket.allow(now, lim)
+	}
+}
+
+// Benchmark single allow calls for golang.org/x/time/rate
+func BenchmarkRate_Allow_Single(b *testing.B) {
+	limiter := rate.NewLimiter(rate.Limit(100), 0)
+	now := time.Now()
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		limiter.AllowN(now, 1)
+	}
+}

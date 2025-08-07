5fd8a38309940f9ebc948c65f975ec064790eaa1
Matt Sherman <mwsherman@gmail.com>
2025-08-07 16:08:24 -0400

    Never mind on bench2md.sh

--- bench2md.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "| Name | Iterations | Time | Allocs |"
-echo "| :------------- | :--------- | :----------------- | :------------ |"
-go test -bench . -benchmem | grep "^Benchmark" | awk '{print "| " $1 " | " $2 " | " $3 " ns/op | " $5 " allocs/op |"}'

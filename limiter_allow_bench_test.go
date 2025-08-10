bca3bcdc68db2c8f63415262a78fe34fa377f66d
Matt Sherman <mwsherman@gmail.com>
2025-08-10 12:16:35 -0400

    Organize BenchmarkLimiter_AllowWithDebug with subtests

--- limiter_allow_bench_test.go
+++ limiter_allow_bench_test.go
@@ -131,67 +131,65 @@ func BenchmarkLimiter_AllowWithDetails(b *testing.B) {
 	})
 }
 
-func BenchmarkLimiter_AllowWithDebug_SingleBucket(b *testing.B) {
-	keyer := func(id int) string {
-		return "allow-debug-single-bucket"
-	}
+func BenchmarkLimiter_AllowWithDebug(b *testing.B) {
+	b.Run("SingleBucket", func(b *testing.B) {
+		keyer := func(id int) string { return "allow-debug-single-bucket" }
 
-	b.Run("SingleLimit", func(b *testing.B) {
-		limit := NewLimit(1000000, time.Second)
-		limiter := NewLimiter(keyer, limit)
-		now := time.Now()
+		b.Run("SingleLimit", func(b *testing.B) {
+			limit := NewLimit(1000000, time.Second)
+			limiter := NewLimiter(keyer, limit)
+			now := time.Now()
 
-		b.ReportAllocs()
+			b.ReportAllocs()
 
-		for b.Loop() {
-			limiter.allowNWithDebug(0, now, 1)
-		}
+			for b.Loop() {
+				limiter.allowNWithDebug(0, now, 1)
+			}
+		})
+
+		b.Run("MultipleLimits", func(b *testing.B) {
+			limit1 := NewLimit(1000000, time.Second)
+			limit2 := NewLimit(500000, time.Second/2)
+			limiter := NewLimiter(keyer, limit1, limit2)
+			now := time.Now()
+
+			b.ReportAllocs()
+
+			for b.Loop() {
+				limiter.allowNWithDebug(0, now, 1)
+			}
+		})
 	})
 
-	b.Run("MultipleLimits", func(b *testing.B) {
-		limit1 := NewLimit(1000000, time.Second)
-		limit2 := NewLimit(500000, time.Second/2)
-		limiter := NewLimiter(keyer, limit1, limit2)
-		now := time.Now()
+	b.Run("MultipleBuckets", func(b *testing.B) {
+		const buckets = 1000
+		keyer := func(id int) int { return id }
 
-		b.ReportAllocs()
+		b.Run("SingleLimit", func(b *testing.B) {
+			limit := NewLimit(1000000, time.Second)
+			limiter := NewLimiter(keyer, limit)
+			now := time.Now()
 
-		for b.Loop() {
-			limiter.allowNWithDebug(0, now, 1)
-		}
-	})
-}
+			b.ResetTimer()
+			b.ReportAllocs()
 
-func BenchmarkLimiter_AllowWithDebug_MultipleBuckets(b *testing.B) {
-	const buckets = 1000
-	keyer := func(id int) int {
-		return id
-	}
+			for i := 0; i < b.N; i++ {
+				limiter.allowNWithDebug(i%buckets, now, 1)
+			}
+		})
 
-	b.Run("SingleLimit", func(b *testing.B) {
-		limit := NewLimit(1000000, time.Second)
-		limiter := NewLimiter(keyer, limit)
-		now := time.Now()
+		b.Run("MultipleLimits", func(b *testing.B) {
+			limit1 := NewLimit(1000000, time.Second)
+			limit2 := NewLimit(500000, time.Second/2)
+			limiter := NewLimiter(keyer, limit1, limit2)
+			now := time.Now()
 
-		b.ResetTimer()
-		b.ReportAllocs()
+			b.ResetTimer()
+			b.ReportAllocs()
 
-		for i := 0; i < b.N; i++ {
-			limiter.allowNWithDebug(i%buckets, now, 1)
-		}
-	})
-
-	b.Run("MultipleLimits", func(b *testing.B) {
-		limit1 := NewLimit(1000000, time.Second)
-		limit2 := NewLimit(500000, time.Second/2)
-		limiter := NewLimiter(keyer, limit1, limit2)
-		now := time.Now()
-
-		b.ResetTimer()
-		b.ReportAllocs()
-
-		for i := 0; i < b.N; i++ {
-			limiter.allowNWithDebug(i%buckets, now, 1)
-		}
+			for i := 0; i < b.N; i++ {
+				limiter.allowNWithDebug(i%buckets, now, 1)
+			}
+		})
 	})
 }

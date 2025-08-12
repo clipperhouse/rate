a5d20d63045048d58a5e21f5bacfb151162abeb5
Matt Sherman <mwsherman@gmail.com>
2025-08-12 15:36:46 -0400

    Add benchmark for Limiters.allowNWithDetails

--- limiters_allow_bench_test.go
+++ limiters_allow_bench_test.go
@@ -214,15 +214,10 @@ func BenchmarkLimiters_AllowWithDetails(b *testing.B) {
 				limiter := NewLimiter(keyFunc, limit)
 				limiters := NewLimiters(limiter)
 				now := time.Now()
-				b.ResetTimer()
 				b.ReportAllocs()
-				i := int64(0)
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						atomic.AddInt64(&i, 1)
-						limiters.allowNWithDetails("single-bucket", now, 1)
-					}
-				})
+				for b.Loop() {
+					limiters.allowNWithDetails("x", now, 1)
+				}
 			})
 			b.Run("MultipleLimits", func(b *testing.B) {
 				limit1 := NewLimit(1_000_000, time.Second)
@@ -230,15 +225,10 @@ func BenchmarkLimiters_AllowWithDetails(b *testing.B) {
 				limiter := NewLimiter(keyFunc, limit1, limit2)
 				limiters := NewLimiters(limiter)
 				now := time.Now()
-				b.ResetTimer()
 				b.ReportAllocs()
-				i := int64(0)
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						atomic.AddInt64(&i, 1)
-						limiters.allowNWithDetails("single-bucket", now, 1)
-					}
-				})
+				for b.Loop() {
+					limiters.allowNWithDetails("x", now, 1)
+				}
 			})
 		})
 
@@ -288,30 +278,20 @@ func BenchmarkLimiters_AllowWithDetails(b *testing.B) {
 					l2 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second))
 					limiters := NewLimiters(l1, l2)
 					now := time.Now()
-					b.ResetTimer()
 					b.ReportAllocs()
-					i := int64(0)
-					b.RunParallel(func(pb *testing.PB) {
-						for pb.Next() {
-							atomic.AddInt64(&i, 1)
-							limiters.allowNWithDetails("single-bucket", now, 1)
-						}
-					})
+					for b.Loop() {
+						limiters.allowNWithDetails("x", now, 1)
+					}
 				})
 				b.Run("MultipleLimits", func(b *testing.B) {
 					l1 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
 					l2 := NewLimiter(keyFunc, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
 					limiters := NewLimiters(l1, l2)
 					now := time.Now()
-					b.ResetTimer()
 					b.ReportAllocs()
-					i := int64(0)
-					b.RunParallel(func(pb *testing.PB) {
-						for pb.Next() {
-							atomic.AddInt64(&i, 1)
-							limiters.allowNWithDetails("single-bucket", now, 1)
-						}
-					})
+					for b.Loop() {
+						limiters.allowNWithDetails("x", now, 1)
+					}
 				})
 			})
 			b.Run("MultipleBuckets", func(b *testing.B) {
@@ -358,30 +338,20 @@ func BenchmarkLimiters_AllowWithDetails(b *testing.B) {
 					l2 := NewLimiter(keyFunc2, NewLimit(1_000_000, time.Second))
 					limiters := NewLimiters(l1, l2)
 					now := time.Now()
-					b.ResetTimer()
 					b.ReportAllocs()
-					i := int64(0)
-					b.RunParallel(func(pb *testing.PB) {
-						for pb.Next() {
-							atomic.AddInt64(&i, 1)
-							limiters.allowNWithDetails("single-bucket", now, 1)
-						}
-					})
+					for b.Loop() {
+						limiters.allowNWithDetails("x", now, 1)
+					}
 				})
 				b.Run("MultipleLimits", func(b *testing.B) {
 					l1 := NewLimiter(keyFunc1, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
 					l2 := NewLimiter(keyFunc2, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
 					limiters := NewLimiters(l1, l2)
 					now := time.Now()
-					b.ResetTimer()
 					b.ReportAllocs()
-					i := int64(0)
-					b.RunParallel(func(pb *testing.PB) {
-						for pb.Next() {
-							atomic.AddInt64(&i, 1)
-							limiters.allowNWithDetails("single-bucket", now, 1)
-						}
-					})
+					for b.Loop() {
+						limiters.allowNWithDetails("x", now, 1)
+					}
 				})
 			})
 			b.Run("MultipleBuckets", func(b *testing.B) {
@@ -415,6 +385,196 @@ func BenchmarkLimiters_AllowWithDetails(b *testing.B) {
 						for pb.Next() {
 							idx := atomic.AddInt64(&i, 1)
 							limiters.allowNWithDetails(idx%buckets, now, 1)
+						}
+					})
+				})
+			})
+		})
+	})
+}
+
+// Hierarchical benchmarks for Limiters.allowNWithDebug providing clearer grouping.
+// Layout matches the other benchmarks for easy comparison.
+func BenchmarkLimiters_AllowWithDebug(b *testing.B) {
+	b.Run("SingleLimiter", func(b *testing.B) {
+		b.Run("SingleBucket", func(b *testing.B) {
+			keyFunc := func(_ string) string { return "single-limiter-single-bucket" }
+			b.Run("SingleLimit", func(b *testing.B) {
+				limit := NewLimit(1_000_000, time.Second)
+				limiter := NewLimiter(keyFunc, limit)
+				limiters := NewLimiters(limiter)
+				now := time.Now()
+				b.ReportAllocs()
+				for b.Loop() {
+					limiters.allowNWithDebug("x", now, 1)
+				}
+			})
+			b.Run("MultipleLimits", func(b *testing.B) {
+				limit1 := NewLimit(1_000_000, time.Second)
+				limit2 := NewLimit(500_000, time.Second/2)
+				limiter := NewLimiter(keyFunc, limit1, limit2)
+				limiters := NewLimiters(limiter)
+				now := time.Now()
+				b.ReportAllocs()
+				for b.Loop() {
+					limiters.allowNWithDebug("x", now, 1)
+				}
+			})
+		})
+
+		b.Run("MultipleBuckets", func(b *testing.B) {
+			const buckets = int64(1000)
+			keyFunc := func(id int64) int64 { return id }
+			b.Run("SingleLimit", func(b *testing.B) {
+				limit := NewLimit(1_000_000, time.Second)
+				limiter := NewLimiter(keyFunc, limit)
+				limiters := NewLimiters(limiter)
+				now := time.Now()
+				b.ResetTimer()
+				b.ReportAllocs()
+				i := int64(0)
+				b.RunParallel(func(pb *testing.PB) {
+					for pb.Next() {
+						idx := atomic.AddInt64(&i, 1)
+						limiters.allowNWithDebug(idx%buckets, now, 1)
+					}
+				})
+			})
+			b.Run("MultipleLimits", func(b *testing.B) {
+				limit1 := NewLimit(1_000_000, time.Second)
+				limit2 := NewLimit(500_000, time.Second/2)
+				limiter := NewLimiter(keyFunc, limit1, limit2)
+				limiters := NewLimiters(limiter)
+				now := time.Now()
+				b.ResetTimer()
+				b.ReportAllocs()
+				i := int64(0)
+				b.RunParallel(func(pb *testing.PB) {
+					for pb.Next() {
+						idx := atomic.AddInt64(&i, 1)
+						limiters.allowNWithDebug(idx%buckets, now, 1)
+					}
+				})
+			})
+		})
+	})
+
+	b.Run("MultipleLimiters", func(b *testing.B) {
+		b.Run("SameKeyer", func(b *testing.B) {
+			keyFunc := func(_ string) string { return "multi-limiters-same-keyFunc-single-bucket" }
+			b.Run("SingleBucket", func(b *testing.B) {
+				b.Run("SingleLimit", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second))
+					l2 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ReportAllocs()
+					for b.Loop() {
+						limiters.allowNWithDebug("x", now, 1)
+					}
+				})
+				b.Run("MultipleLimits", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
+					l2 := NewLimiter(keyFunc, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ReportAllocs()
+					for b.Loop() {
+						limiters.allowNWithDebug("x", now, 1)
+					}
+				})
+			})
+			b.Run("MultipleBuckets", func(b *testing.B) {
+				const buckets = int64(1000)
+				keyFunc := func(id int64) int64 { return id }
+				b.Run("SingleLimit", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second))
+					l2 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ResetTimer()
+					b.ReportAllocs()
+					i := int64(0)
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							idx := atomic.AddInt64(&i, 1)
+							limiters.allowNWithDebug(idx%buckets, now, 1)
+						}
+					})
+				})
+				b.Run("MultipleLimits", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
+					l2 := NewLimiter(keyFunc, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ResetTimer()
+					b.ReportAllocs()
+					i := int64(0)
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							idx := atomic.AddInt64(&i, 1)
+							limiters.allowNWithDebug(idx%buckets, now, 1)
+						}
+					})
+				})
+			})
+		})
+		b.Run("DifferentKeyers", func(b *testing.B) {
+			b.Run("SingleBucket", func(b *testing.B) {
+				keyFunc1 := func(_ string) string { return "multi-limiters-different-keyFunc1-single-bucket" }
+				keyFunc2 := func(_ string) string { return "multi-limiters-different-keyFunc2-single-bucket" }
+				b.Run("SingleLimit", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc1, NewLimit(1_000_000, time.Second))
+					l2 := NewLimiter(keyFunc2, NewLimit(1_000_000, time.Second))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ReportAllocs()
+					for b.Loop() {
+						limiters.allowNWithDebug("x", now, 1)
+					}
+				})
+				b.Run("MultipleLimits", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc1, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
+					l2 := NewLimiter(keyFunc2, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ReportAllocs()
+					for b.Loop() {
+						limiters.allowNWithDebug("x", now, 1)
+					}
+				})
+			})
+			b.Run("MultipleBuckets", func(b *testing.B) {
+				const buckets = int64(1000)
+				keyFunc1 := func(id int64) int64 { return id }
+				keyFunc2 := func(id int64) int64 { return id * 131 }
+				b.Run("SingleLimit", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc1, NewLimit(1_000_000, time.Second))
+					l2 := NewLimiter(keyFunc2, NewLimit(1_000_000, time.Second))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ResetTimer()
+					b.ReportAllocs()
+					i := int64(0)
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							idx := atomic.AddInt64(&i, 1)
+							limiters.allowNWithDebug(idx%buckets, now, 1)
+						}
+					})
+				})
+				b.Run("MultipleLimits", func(b *testing.B) {
+					l1 := NewLimiter(keyFunc1, NewLimit(1_000_000, time.Second), NewLimit(500_000, time.Second/2))
+					l2 := NewLimiter(keyFunc2, NewLimit(750_000, time.Second), NewLimit(400_000, time.Second/2))
+					limiters := NewLimiters(l1, l2)
+					now := time.Now()
+					b.ResetTimer()
+					b.ReportAllocs()
+					i := int64(0)
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							idx := atomic.AddInt64(&i, 1)
+							limiters.allowNWithDebug(idx%buckets, now, 1)
 						}
 					})
 				})
